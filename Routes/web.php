6bca29555991aeeefad4752666d751674eddf04a
Sajad Rahmani <38728381+SrDev93@users.noreply.github.com>
2024-10-29 10:46:28 +0330

    Update web.php

--- Routes/web.php
+++ Routes/web.php
@@ -12,6 +12,6 @@
 */
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('panel')->group(function() {
+Route::prefix('admin')->group(function() {
     Route::resource('faq', 'FaqController');
 });
